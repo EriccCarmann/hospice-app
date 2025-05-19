821ffafba7eddf500f7d305e90447f1b00cdeea6
Sasha Manyonok <83176347+EriccCarmann@users.noreply.github.com>
2025-05-19 12:14:37 +0200

    autofill partly works

--- HospiceApp/HospiceApp/ViewModels/SearchDiseasesViewModel.cs
+++ HospiceApp/HospiceApp/ViewModels/SearchDiseasesViewModel.cs
@@ -7,63 +7,50 @@ using zoft.MauiExtensions.Core.Extensions;
 
 namespace HospiceApp.ViewModels;
 
-public partial class ListItem : ObservableObject
+public partial class DiseaseName : ObservableObject
 {
     [ObservableProperty]
-    public string _group;
-
-    [ObservableProperty]
-    public string _country;
+    public string _name;
 }
 
 public partial class SearchDiseasesViewModel : ObservableObject
 {
-    public readonly List<ListItem> Teams  = new List<ListItem>()
-    {
-        new ListItem() {Group = "Group 1", Country = "Country 1"},
-        new ListItem() {Group = "Group 1", Country = "Country 1"},
-        new ListItem() {Group = "Group 1", Country = "Country 1"},
-    };
+    public readonly List<DiseaseName> DiseaseNames  = new List<DiseaseName>();
     
     [ObservableProperty]
-    private ObservableCollection<ListItem> _filteredList;
+    private ObservableCollection<DiseaseName> _filteredList;
 
     [ObservableProperty]
-    private ListItem _selectedItem;
+    private DiseaseName _selectedItem;
+    
+    
+    
+    
+    
+    
 
     [ObservableProperty]
     public int _cursorPosition;
     
-    public List<Disease> DiseasesAll { get; } = new List<Disease>();//delete
+
     
-    // [ObservableProperty]
-    // private ObservableCollection<string> _filteredList = new ();
-    //
-    // [ObservableProperty]
-    // private string _selectedItem;
-    //
-    // [ObservableProperty]
-    // private int _cursorPosition;
-    //
-    // public IAsyncRelayCommand<string> TextChangedCommand { get; }
-    //
-    // private readonly IStrapiService _strapiService;
-    //
+    private readonly IStrapiService _strapiService;
+    
      public ObservableCollection<Disease> Diseases { get; } = new ();
-    //
-    // [ObservableProperty] private string _name;
+    
+    [ObservableProperty] private string _name;
      public IAsyncRelayCommand SearchCommand { get; }
     
     public SearchDiseasesViewModel(IStrapiService strapiService)
     {
-        //_strapiService = strapiService;
+        _strapiService = strapiService;
+
+        GetNames();
         
-        FilteredList = new(Teams);
-        SelectedItem = null;
-        
-        //SearchCommand = new AsyncRelayCommand(SearchDiseases);
+        SearchCommand = new AsyncRelayCommand(SearchDiseases);
         //TextChangedCommand = new AsyncRelayCommand<string>(OnTextChangedAsync);
     }
+    
     [RelayCommand]
     private void TextChanged(string text)
     {
@@ -74,86 +61,31 @@ public partial class SearchDiseasesViewModel : ObservableObject
         SelectedItem = null;
         FilteredList.Clear();
 
-        FilteredList.AddRange(Teams.Where(t => t.Group.Contains(filter, StringComparison.CurrentCultureIgnoreCase) ||
-                                               t.Country.Contains(filter, StringComparison.CurrentCultureIgnoreCase)));
+        FilteredList.AddRange(DiseaseNames.Where(t => t.Name.Contains(filter, StringComparison.CurrentCultureIgnoreCase)));
     }
-
-    // private async Task OnTextChangedAsync(string text)
-    // {
-    //     MainThread.BeginInvokeOnMainThread(() =>
-    //     {
-    //         FilteredList.Clear();
-    //         SelectedItem = null;
-    //     });
-    //
-    //     if (string.IsNullOrWhiteSpace(text))
-    //         return;
-    //
-    //     // fetch matching diseases by name
-    //     var matches = await _strapiService.GetDiseasesByNameAsync(text);
-    //
-    //     // Update the filtered list on the main thread
-    //     MainThread.BeginInvokeOnMainThread(() =>
-    //     {
-    //         foreach (var disease in matches)
-    //         {
-    //             FilteredList.Add(disease.Name);
-    //         }
-    //     });
-    // }
-
-    // private async Task<List<string>> GetNames()
-    // {
-    //     var diseases = await _strapiService.GetDiseasesAsync();
-    //     var stringList = new List<string>();
-    //     
-    //     foreach (var disease in diseases)
-    //     {
-    //         stringList.Add(disease.Name);;
-    //     }
-    //     
-    //    return stringList;
-    // }
     
-    // private async Task GetDiseases()
-    // {
-    //     Diseases.AddRange(await _strapiService.GetDiseasesAsync());
-    //     
-    //     foreach (var disease in Diseases)
-    //     {
-    //         FilteredList.Add(disease.Name);
-    //     }
-    //     
-    //     MainThread.BeginInvokeOnMainThread(() =>
-    //     {
-    //         DiseasesAll.Clear();
-    //         FilteredList.Clear();
-    //         
-    //         foreach (var disease in Diseases)
-    //         {
-    //             FilteredList.Add(disease.Name);
-    //         }
-    //     });
-    // }
-    //
-    // private void FilterList(string filter)
-    // {
-    //     SelectedItem = null;
-    //     FilteredList.Clear();
-    //     FilteredList.Add(filter);
-    //     // FilteredList.AddRange(Teams.Where(t => t.Group.Contains(filter, StringComparison.CurrentCultureIgnoreCase) ||
-    //     //                                        t.Country.Contains(filter, StringComparison.CurrentCultureIgnoreCase)));
-    // }
-    //
-    // private async Task SearchDiseases()
-    // {
-    //     Diseases.Clear();
-    //
-    //     var desieses = await _strapiService.GetDiseasesByNameAsync(Name);
-    //     
-    //     foreach (var desiese in desieses)
-    //     {
-    //         Diseases.Add(desiese);
-    //     }
-    // }
+    private async Task SearchDiseases()
+    {
+        Diseases.Clear();
+    
+        var diseases = await _strapiService.GetDiseasesByNameAsync(Name);
+        
+        foreach (var disease in diseases)
+        {
+            Diseases.Add(disease);
+        }
+    }
+    
+    private async Task GetNames()
+    {
+        var diseases = await _strapiService.GetDiseasesAsync();
+        
+        foreach (var disease in diseases)
+        {
+            DiseaseNames.Add(new DiseaseName{Name = disease.Name});
+        }
+        
+        FilteredList = new(DiseaseNames);
+        SelectedItem = null;
+    }
 }
