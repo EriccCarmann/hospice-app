5889ffb6abe324fb40e2fb46683385b330ba883a
Sasha Manyonok <83176347+EriccCarmann@users.noreply.github.com>
2025-05-19 11:14:14 +0200

    added default searches

--- HospiceApp/HospiceApp/ViewModels/SearchDiseasesViewModel.cs
+++ HospiceApp/HospiceApp/ViewModels/SearchDiseasesViewModel.cs
@@ -7,64 +7,100 @@ using zoft.MauiExtensions.Core.Extensions;
 
 namespace HospiceApp.ViewModels;
 
+public partial class ListItem : ObservableObject
+{
+    [ObservableProperty]
+    public string _group;
+
+    [ObservableProperty]
+    public string _country;
+}
+
 public partial class SearchDiseasesViewModel : ObservableObject
 {
+    public readonly List<ListItem> Teams  = new List<ListItem>()
+    {
+        new ListItem() {Group = "Group 1", Country = "Country 1"},
+        new ListItem() {Group = "Group 1", Country = "Country 1"},
+        new ListItem() {Group = "Group 1", Country = "Country 1"},
+    };
+    
+    [ObservableProperty]
+    private ObservableCollection<ListItem> _filteredList;
+
+    [ObservableProperty]
+    private ListItem _selectedItem;
+
+    [ObservableProperty]
+    public int _cursorPosition;
+    
     public List<Disease> DiseasesAll { get; } = new List<Disease>();//delete
     
-    [ObservableProperty]
-    private ObservableCollection<string> _filteredList = new ();
-
-    [ObservableProperty]
-    private string _selectedItem;
-
-    [ObservableProperty]
-    private int _cursorPosition;
-    
-    public IAsyncRelayCommand<string> TextChangedCommand { get; }
-    
-    private readonly IStrapiService _strapiService;
-
-    public ObservableCollection<Disease> Diseases { get; } = new ();
-    
-    [ObservableProperty] private string _name;
-    public IAsyncRelayCommand SearchCommand { get; }
+    // [ObservableProperty]
+    // private ObservableCollection<string> _filteredList = new ();
+    //
+    // [ObservableProperty]
+    // private string _selectedItem;
+    //
+    // [ObservableProperty]
+    // private int _cursorPosition;
+    //
+    // public IAsyncRelayCommand<string> TextChangedCommand { get; }
+    //
+    // private readonly IStrapiService _strapiService;
+    //
+     public ObservableCollection<Disease> Diseases { get; } = new ();
+    //
+    // [ObservableProperty] private string _name;
+     public IAsyncRelayCommand SearchCommand { get; }
     
     public SearchDiseasesViewModel(IStrapiService strapiService)
     {
-        _strapiService = strapiService;
-
-        MainThread.BeginInvokeOnMainThread(() =>
-        {
-            GetDiseases();
-        });
+        //_strapiService = strapiService;
         
-        SearchCommand = new AsyncRelayCommand(SearchDiseases);
-        TextChangedCommand = new AsyncRelayCommand<string>(OnTextChangedAsync);
+        FilteredList = new(Teams);
+        SelectedItem = null;
+        
+        //SearchCommand = new AsyncRelayCommand(SearchDiseases);
+        //TextChangedCommand = new AsyncRelayCommand<string>(OnTextChangedAsync);
     }
-
-    private async Task OnTextChangedAsync(string text)
+    [RelayCommand]
+    private void TextChanged(string text)
     {
-        MainThread.BeginInvokeOnMainThread(() =>
-        {
-            FilteredList.Clear();
-            SelectedItem = null;
-        });
-
-        if (string.IsNullOrWhiteSpace(text))
-            return;
-
-        // fetch matching diseases by name
-        var matches = await _strapiService.GetDiseasesByNameAsync(text);
-
-        // Update the filtered list on the main thread
-        MainThread.BeginInvokeOnMainThread(() =>
-        {
-            foreach (var disease in matches)
-            {
-                FilteredList.Add(disease.Name);
-            }
-        });
+        FilterList(text);
     }
+    private void FilterList(string filter)
+    {
+        SelectedItem = null;
+        FilteredList.Clear();
+
+        FilteredList.AddRange(Teams.Where(t => t.Group.Contains(filter, StringComparison.CurrentCultureIgnoreCase) ||
+                                               t.Country.Contains(filter, StringComparison.CurrentCultureIgnoreCase)));
+    }
+
+    // private async Task OnTextChangedAsync(string text)
+    // {
+    //     MainThread.BeginInvokeOnMainThread(() =>
+    //     {
+    //         FilteredList.Clear();
+    //         SelectedItem = null;
+    //     });
+    //
+    //     if (string.IsNullOrWhiteSpace(text))
+    //         return;
+    //
+    //     // fetch matching diseases by name
+    //     var matches = await _strapiService.GetDiseasesByNameAsync(text);
+    //
+    //     // Update the filtered list on the main thread
+    //     MainThread.BeginInvokeOnMainThread(() =>
+    //     {
+    //         foreach (var disease in matches)
+    //         {
+    //             FilteredList.Add(disease.Name);
+    //         }
+    //     });
+    // }
 
     // private async Task<List<string>> GetNames()
     // {
@@ -79,45 +115,45 @@ public partial class SearchDiseasesViewModel : ObservableObject
     //    return stringList;
     // }
     
-    private async Task GetDiseases()
-    {
-        // Diseases.AddRange(await _strapiService.GetDiseasesAsync());
-        //
-        // foreach (var disease in Diseases)
-        // {
-        //     FilteredList.Add(disease.Name);
-        // }
-        
-        // MainThread.BeginInvokeOnMainThread(() =>
-        // {
-        //     DiseasesAll.Clear();
-        //     FilteredList.Clear();
-        //     
-        //     foreach (var disease in diseasesList)
-        //     {
-        //         DiseasesAll.Add(disease);
-        //     }
-        // });
-    }
-    
-    private void FilterList(string filter)
-    {
-        SelectedItem = null;
-        FilteredList.Clear();
-        FilteredList.Add(filter);
-        // FilteredList.AddRange(Teams.Where(t => t.Group.Contains(filter, StringComparison.CurrentCultureIgnoreCase) ||
-        //                                        t.Country.Contains(filter, StringComparison.CurrentCultureIgnoreCase)));
-    }
-    
-    private async Task SearchDiseases()
-    {
-        Diseases.Clear();
-
-        var desieses = await _strapiService.GetDiseasesByNameAsync(Name);
-        
-        foreach (var desiese in desieses)
-        {
-            Diseases.Add(desiese);
-        }
-    }
+    // private async Task GetDiseases()
+    // {
+    //     Diseases.AddRange(await _strapiService.GetDiseasesAsync());
+    //     
+    //     foreach (var disease in Diseases)
+    //     {
+    //         FilteredList.Add(disease.Name);
+    //     }
+    //     
+    //     MainThread.BeginInvokeOnMainThread(() =>
+    //     {
+    //         DiseasesAll.Clear();
+    //         FilteredList.Clear();
+    //         
+    //         foreach (var disease in Diseases)
+    //         {
+    //             FilteredList.Add(disease.Name);
+    //         }
+    //     });
+    // }
+    //
+    // private void FilterList(string filter)
+    // {
+    //     SelectedItem = null;
+    //     FilteredList.Clear();
+    //     FilteredList.Add(filter);
+    //     // FilteredList.AddRange(Teams.Where(t => t.Group.Contains(filter, StringComparison.CurrentCultureIgnoreCase) ||
+    //     //                                        t.Country.Contains(filter, StringComparison.CurrentCultureIgnoreCase)));
+    // }
+    //
+    // private async Task SearchDiseases()
+    // {
+    //     Diseases.Clear();
+    //
+    //     var desieses = await _strapiService.GetDiseasesByNameAsync(Name);
+    //     
+    //     foreach (var desiese in desieses)
+    //     {
+    //         Diseases.Add(desiese);
+    //     }
+    // }
 }
